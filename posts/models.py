6ba912c69afce1e113978dccb86941753c57dc0d
Evan Jesty <evanjesty@gmail.com>
2018-02-14 01:59:03 -0500

    Fixed models for Author

--- posts/models.py
+++ posts/models.py
@@ -13,13 +13,13 @@ class Post(models.Model):
     preview = models.TextField() # Preview, shown when: searching for posts or at home
     published = models.DateTimeField('date published')
     post_id = models.AutoField(primary_key=True)
-    tags = models.ManyToManyField(Tag, blank=True)
-    author = Author
+    tags = models.ManyToManyField('Tag', blank=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True )
     def __str__(self):
         return self.title
 
 class Author(models.Model):
     name = models.CharField(max_length=200)
-    posts = models.ManyToManyField(Post, blank=True)
+    #posts = models.ManyToManyField('Post', blank=True)
     def __str__(self):
         return self.name
