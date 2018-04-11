3d9255fc8c1bcf4089f5efce7f6080d18733f886
Evan Jesty <evanjesty@gmail.com>
2018-04-11 01:21:18 -0400

    Began implemeting search posts

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('ajax/<int:page>', views.ajax, name='ajax'),
     path('<int:pid>', views.getPost, name='getPost'),
     path('<int:pid>/<slug:slug>', views.getPost, name='getPost'),
-    path('tags/<str:tag>', views.postsByTag, name='postsByTag')
+    path('tags/<str:tag>', views.postsByTag, name='postsByTag'),
+    path('search', views.search, name='search'),
+    path('search/<str:type>', views.search, name='search'),
+    path('search/<str:type>/<str:searchtext>', views.search, name='search'),
+    
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.template import loader
+from django.core.exceptions import ObjectDoesNotExist
 
 from posts.models import Post, Tag
 
@@ -59,3 +60,36 @@ def getPost(request, pid, slug=None):
      'tags': p.tags.all()
      }
     return render(request, 'posts/post.html', context)
+
+def search(request, type=None, searchtext=None, amount=5, page=1):
+    print(type)
+    print(searchtext)
+    if (type == 'tag'):
+        print('?')
+        try:
+            actualTag = Tag.objects.get(tag=searchtext)
+        except ObjectDoesNotExist:
+            actualTag = None
+        
+        posts = Post.objects.filter(tags=actualTag) # todo: very basic, use regex (tag1&tag2&tag3)
+        total = len(posts)
+        posts = posts[((page - 1) * amount):(amount + ((page - 1) * amount))]
+        context = {
+            'hastag': True,
+            'tag': actualTag,
+            'latest_posts_list': posts,
+            'more_posts': (len(posts) >= amount),
+            'no_posts': (len(posts) == 0),
+            'is_home': False,
+            'amount': total
+        }
+        return render(request, 'posts/search.html', context)
+    else:
+        context = {
+            'hastag': False,
+            'none': True,
+            'no_posts': True,
+            'is_home': False,
+            'amount': 0
+        }
+        return render(request, 'posts/search.html', context)
